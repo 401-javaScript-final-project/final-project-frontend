1d632c16ba937cc24a12705c928df8bd7277f918
Adrienne <adrienneeaston@gmail.com>
2019-11-02 11:02:05 -0700

    more setup

---
+++ tipping-app/__testing__/tipping-app.test.js
@@ -0,0 +1,23 @@
+const app = require("../App.js");
+
+describe("This is an", () => {
+  it("this should ", () => {
+
+  });
+
+  it("this should ", () => {
+
+  });
+
+  it("this should ", () => {
+
+  });
+
+  it("this should ", () => {
+
+  });
+
+  it("this should ", () => {
+
+  });
+});
