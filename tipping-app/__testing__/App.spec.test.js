56a64f9b365c21d6eb0b79b96ea1aa22246454eb
Adrienne <adrienneeaston@gmail.com>
2019-11-02 16:29:11 -0700

    firebase mock testing setup

---
+++ tipping-app/__testing__/App.spec.test.js
@@ -0,0 +1,9 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
+
+it('App should render correctly', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
