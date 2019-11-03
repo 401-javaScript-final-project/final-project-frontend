63f32aedfb6260fe2f343c2c57a47097cacef1f1
Adrienne <adrienneeaston@gmail.com>
2019-11-02 17:12:38 -0700

    trying mock firebase

--- tipping-app/__testing__/App.spec.test.js
+++ tipping-app/__testing__/App.spec.test.js
@@ -3,6 +3,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
+console.log("Hello!");
+
 it('App should render correctly', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
